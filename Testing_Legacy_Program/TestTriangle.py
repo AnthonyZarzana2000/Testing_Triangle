5bcd35118301eca9130a57162242fce126fd68d4
AnthonyZarzana2000 <azarzana@stevens.edu>
2024-10-23 19:42:31 -0400

    Update TestTriangle.py

--- Testing_Legacy_Program/TestTriangle.py
+++ Testing_Legacy_Program/TestTriangle.py
@@ -1,18 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Updated Jan 21, 2018
-The primary goal of this file is to demonstrate a simple unittest implementation
-
-@author: jrr
-@author: rk
-"""
-
 import unittest
 
 from Triangle import classifyTriangle
-
-# This code implements the unit test functionality
-# https://docs.python.org/3/library/unittest.html has a nice description of the framework
 
 class TestTriangles(unittest.TestCase):
     test_results = []  
@@ -27,26 +15,6 @@ class TestTriangles(unittest.TestCase):
             'Actual Result': actual_result,
             'Pass or Fail': 'Pass' if passed else 'Fail'
         })
-
-#################################################################################################
-    # def testRightTriangleA(self): 
-    #     self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
-
-    # # def testRightTriangleB(self): 
-    # #     self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
-
-    # def testRightTriangleB(self): 
-    #     self.assertEqual(classifyTriangle(5,3,4),'Scalene','5,3,4 is a Scalene triangle')
-        
-    # def testEquilateralTriangles(self): 
-    #     self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
-#################################################################################################
-
-
-    # Above are the original tests that we were using to test the Triangle code
-    # Below are the new Tests that I have created to test the program
-    # The program prints out a text file that you will notice in the repo that shows the results of the test in
-    # the same orderly table layout that was shown in the instructions of the assignment.
 
     def testRightTriangleA(self):
         self.run_test('1', 3, 4, 5, 'Right')
